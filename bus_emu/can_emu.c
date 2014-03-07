d21147bdd81993d462bc14d2c5e6f70017532e43
hu <hu9700@users.noreply.github.com>
2014-03-07 12:14:16 -0600

    make main function into start_can_emu

--- bus_emu/can_emu.c
+++ bus_emu/can_emu.c
@@ -6,6 +6,12 @@
 #include <sys/stat.h>//stat()
 #include <dirent.h>//opendir()
 #include <sys/types.h>
+#include <string.h>
+#include <fcntl.h>//mknod
+#include "../include/can_bus.h"
+
+static char bus_path[PATH_MAX];
+static char nodes_path[PATH_MAX];
 
 char * get_image_path(pid_t pid, char *proc_dir_buff, size_t size)//sizeof proc_dir_buff should be PATH_MAX
 {
@@ -57,7 +63,7 @@ int arg_handler(int argc, char **argv)
 		
 	if(argc >= 2)
 	{
-		node_num = atoi(argv[2]);
+		node_num = atoi(argv[1]);
 	}
 	else
 	{
@@ -68,6 +74,8 @@ int arg_handler(int argc, char **argv)
 int check_file(int node_num)
 {//check node file, check bus file
 	char process_image_dir[PATH_MAX];
+	char bus_folder[PATH_MAX];
+	char nodes_folder[PATH_MAX];	
 	char *process_image_folder = NULL;
 	void *res;
 	int rval = 0;
@@ -113,8 +121,6 @@ int check_file(int node_num)
 		
 		if(rval == 0)
 		{//check bus folder and nodes folder exist
-			char bus_folder[PATH_MAX];
-			char nodes_folder[PATH_MAX];
 			DIR *bus_dir = NULL;
 			DIR *nodes_dir = NULL;
 			
@@ -132,8 +138,15 @@ int check_file(int node_num)
 				perror("check bus_dir");
 				if(errno == ENOENT)
 				{
-					rval = mkdir(bus_dir, 0777);
-					printf("mkdir %s", bus_dir);
+					rval = mkdir(bus_folder, 0777);
+					if(rval == 0)
+					{
+						printf("mkdir %s", bus_folder);
+					}
+					else
+					{
+						perror("make bus folder");
+					}
 				}
 				else
 				{
@@ -142,6 +155,7 @@ int check_file(int node_num)
 			}
 			else
 			{
+				printf("exist %s\n", bus_folder);
 				closedir(bus_dir);
 			}
 			
@@ -150,8 +164,15 @@ int check_file(int node_num)
 				perror("check nodes_dir");
 				if(errno == ENOENT)
 				{
-					rval = mkdir(nodes_dir, 0777);
-					printf("mkdir %s", nodes_dir);
+					rval = mkdir(nodes_folder, 0777);
+					if(rval == 0)
+					{
+						printf("mkdir %s", nodes_folder);
+					}
+					else
+					{
+						perror("make nodes folder");
+					}
 				}
 				else
 				{
@@ -160,26 +181,140 @@ int check_file(int node_num)
 			}
 			else
 			{
+				printf("exist %s\n", nodes_folder);
 				closedir(nodes_dir);
 			}
 			
 			if(rval == 0)		
 			{//check nodes and bus files
+				char bus_file[PATH_MAX];
+				DIR *bus_dir = NULL;
+				memcpy(bus_file, bus_folder, sizeof(bus_file));
+				strncat(bus_file, "bus", sizeof(bus_file));
+				bus_dir = opendir(bus_file);
+				if(bus_dir == NULL)
+				{
+					perror("bus file");
+					if(errno == ENOENT)
+					{
+						rval = mknod(bus_file, S_IFREG, 0);
+						if(rval == 0)
+						{
+							printf("make bus file\n");
+							chmod(bus_file, 0666);
+						}
+						else
+						{
+							perror("make bus file");
+						}
+					}
+					else
+					{
+						rval = -1;
+					}
+				}
+				else
+				{
+					printf("exist %s\n", bus_file);
+					closedir(bus_dir);
+				}
 				
+				if(rval == 0)
+				{//check node file
+					int count = 0;
+					char nodes_file[PATH_MAX];
+					
+					for(count = 0; count < node_num; count ++)
+					{
+						DIR *nodes_dir = NULL;
+						memset(nodes_file, 0, sizeof(nodes_file));
+						snprintf(nodes_file, sizeof(nodes_file), "%snode_%d", nodes_folder, count);
+						nodes_dir = opendir(nodes_file);
+						if(nodes_dir == NULL)
+						{
+							perror("check nodes file");
+							if(errno == ENOENT)
+							{
+								rval = mknod(nodes_file, S_IFIFO, 0);
+								if(rval == 0)
+								{
+									printf("make nodes_%d file\n", count);
+									chmod(nodes_file, 0666);
+								}							
+								else
+								{
+									perror("make nodes");
+								}
+							}
+							else
+							{
+								rval = -1;
+							}						
+						}
+						else
+						{
+							printf("exist %s\n", nodes_file);
+							closedir(nodes_dir);
+						}
+					}//loop to check nodes file
+				}
 			}
 		}
 	}
+	
+	if(rval == 0)
+	{
+		memset(bus_path, 0, sizeof(bus_path));
+		memset(nodes_path, 0, sizeof(nodes_path));
+		snprintf(bus_path, sizeof(bus_path), "%sbus", bus_folder);
+		snprintf(nodes_path, sizeof(nodes_path), "%snode_", nodes_folder);
+	}
+	
+	return rval;
 }
 
-int main(int argc, char **argv)
+//int main(int argc, char **argv)
+int start_can_emu(int node_num)
 {
-	int node_num = 0;
+	//int node_num = 0;
+	int rval = 0;
 	
-	node_num = arg_handler(argc, argv);
+	//node_num = arg_handler(argc, argv);
 	if(node_num < 1)
 	{
 		exit(-1);
 	}
 	
-	umask(0);
+	umask(0);	
+	rval = check_file(node_num);//create bus file and node file
+	
+	if(rval != 0)
+	{//fail
+		printf("\nfilesystem create FAIL!!!\n");
+	}
+	else
+	{//success
+		printf("\nfilesystem create SUCCESS!!!\n");
+		
+		//loop to wait and receive data from fifo
+		{
+			int fifo_fdread_list[node_num];
+			int fifo_fdwrite_list[node_num];
+			int bus_fd;
+			int count;
+			char node_dir[PATH_MAX];
+			
+			memset(fifo_list, 0, sizeof(int) * node_num);
+			
+			for(count = 0; count < node_num; count ++)
+			{
+				memcpy(node_dir, nodes_path, sizeof(node_dir));
+				snprintf(node_dir, sizeof(node_dir), "%snode_%d", node_dir, count);
+				fifo_fdread_list[count] = open(node_dir, O_RDONLY);
+				fifo_fdwrite_list[count] = open(node_dir, O_WRONLY);
+			}
+		}
+	}//file system created
+	
+	return rval;
 }
